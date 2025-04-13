43e8758ed048571af5d60062e0b0ee24887589ce
abderrahm4ne <abderrahmaneita@gmail.com>
2025-04-13 16:44:04 +0100

    dad

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import NavBar from './Components/NavBar'
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 function App() {
 
-  return (
-    <>
-      
-    </>
+        {/*==== ROUTER ====*/} 
+          const router = createBrowserRouter(
+            createRoutesFromElements(
+            <Route path="/" >
+              <Route index element={<NavBar />}>
+                  { /*<Route path="home" element={<HomePage />} />
+                  <Route path="products" element={ <ProductPage />} />
+                  <Route path="about" element={<AboutPage />} />
+                  <Route path="contact" element={<ContactPage />} />
+                  <Route path="cart" element={<CartPage />} /> */}
+              </Route>
+            </Route>
+            )
+          )
+        {/*==== ROUTER ====*/}   
+  return(
+    <div style={{height:"100vh"}} className='w-[100%] bg-amber-100'>
+      <NavBar />
+    </div>
   )
 }
 
