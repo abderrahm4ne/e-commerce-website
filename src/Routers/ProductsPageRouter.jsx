59810630fbcf7478741d1f973d92ecb9dc665af1
abderrahm4ne <abderrahmaneita@gmail.com>
2025-05-21 15:38:24 +0100

    sdasd

--- src/Routers/ProductsPageRouter.jsx
+++ src/Routers/ProductsPageRouter.jsx
@@ -2,7 +2,7 @@ import ProductsPage from "../Components/ProductsPage";
 
 export default function ProductsPageRouter() { 
     return (
-        <div style={{backgroundImage: "linear-gradient(120deg, #fbf2f2, #3B3B3B 60%)",
+        <div style={{backgrundImage: "linear-gradient(120deg, #fbf2f2, #3B3B3B 60%)",
             padding:"20px"}}>
             <ProductsPage />
         </div>     
