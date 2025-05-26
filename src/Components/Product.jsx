5199d995bf4b47e7d7db175a51c8158858f58fd6
abderrahm4ne <abderrahmaneita@gmail.com>
2025-05-26 19:37:30 +0100

    adding the side of s p page

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -9,6 +9,10 @@ import { useParams } from 'react-router-dom';
 // components
 import { ProductContext } from '../Context/ProductsContext';
 
+
+// ROUTER 
+import { NavLink } from 'react-router-dom';
+
 export default function Product() {
 
     // CONTEXT
@@ -16,42 +20,28 @@ export default function Product() {
     const { id } = useParams();
     return ( 
             
-        <div className='flex flex-col items-center justify-center gap-10'>
-      {loading ? (
-        <Typography variant="h2" style={{ fontFamily: "Oswald", color: "#3B3B3B", textAlign: "center" }}>
-          Loading...
-        </Typography>
-      ) : (
-        <>
-          <Typography
-            variant="h3"
-            gutterBottom={false}
-            sx={{
-              color: "#3B3B3B",
-              fontWeight: "bold",
-              fontFamily: "Oswald",
-              textAlign: "center",
-              textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)"
-            }}
-          >
-            PRODUCT DETAILS
-          </Typography>
-          <div className='flex flex-col items-center justify-center gap-10'>
-            <Typography variant='h4' style={{ fontFamily: "Oswald", color: "#3B3B3B", textAlign: "center" }}>
-              {myProducts[id].name}          
-            </Typography>
-            <Typography variant='body1' style={{ fontFamily: "Oswald", color: "#3B3B3B", textAlign: "center" }}>
-                  {myProducts[id].article}
-            </Typography>
-            <Typography variant='body1' style={{ fontFamily: "Oswald", color: "#3B3B3B", textAlign: "center" }}>
-                  {myProducts[id].category}
-            </Typography>
-            <Typography variant='body1' style={{ fontFamily: "Oswald", color: "#3B3B3B", textAlign: "center" }}>
-                  {myProducts[id].color}
-            </Typography>
-          </div>
-        </>
-      )}
-    </div>
+        <div className=' flex flex-row w-[98vw]'>
+
+            { /* OTHER PRODUCTS NAME */ }
+
+            <div className='flex flex-col text-black-900 lg:w-[17vw] p-3 border-r-3 border-black-900 mt-3.5 mb-3.5'>
+
+                <Typography variant='h5' className=' pl-3 text-[#3B3B3B]' sx={{fontWeight:"bold"}}>
+                  Others Products
+                </Typography>
+
+                <div className='flex flex-col justify-center'>
+                    {myProducts.map((product) => (
+                        <NavLink to={`/products/${product.id}`} key={product.id} className='p-2 m-2 underline' >
+                            <Typography variant='h7' sx={{color:"#3B3B3B", fontWeight:"bold"}}>{product.article}</Typography>
+                        </NavLink>
+                    ))}
+                </div>
+            </div>
+            
+            {/* OTHER PRODUCTS NAME */ }
+
+
+        </div>
           
     )}
