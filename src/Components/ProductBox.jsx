158c1b496053a41b7ad159356d56aac6b9093b4f
abderrahm4ne <abderrahmaneita@gmail.com>
2025-06-05 17:11:39 +0100

    zxczx

--- src/Components/ProductBox.jsx
+++ src/Components/ProductBox.jsx
@@ -6,7 +6,7 @@ import Button from '@mui/material/Button';
 
 export function ProductBox({ product }) {
   return (
-    <div className="flex flex-col items-center justify-center  rounded-lg border-1 transition duration-300 ease-in-out hover:border-3 hover:shadow-lg p-3 w-[19vw] h-[100%]" >
+    <div className="flex flex-col items-center justify-center  rounded-lg border-1 transition duration-300 ease-in-out hover:border-3 hover:shadow-lg p-3 bg-amber-600" >
 
       <img 
           src={`../public/DB/${product.id}/productPage.png`} 
@@ -15,19 +15,16 @@ export function ProductBox({ product }) {
       </img> 
 
 
-      <div className="flex flex-col items-center justify-center gap-3">
-        <Typography 
-          variant="h5" 
-          gutterBottom={false} 
+      <div className="flex flex-col items-center justify-center gap-3 bg-amber-500">
+        <div 
+          className="h-[60px] px-2 w-[100%] 2xl:text-xl lg:text-sm md:text-sm sm:text-sm text-sm"
           style={{ fontFamily: "Oswald", color: "#3B3B3B", textAlign: "center" }}
-          className="h-[60px] px-2 w-[100%]"
-          >
+        >
           {product.name}
-        </Typography>
 
-        <button className="border-1 w-[100%] border-[#3b3b3b] bg-[#3B3B3B] text-white py-4 rounded text-xl" sx={{fontFamily: "Oswald"}}>
-          GET NOW
-        </button>
+        </div>
+
+        
 
       </div>
     </div>
